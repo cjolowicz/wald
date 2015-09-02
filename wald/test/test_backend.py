fe100ab2eb0923a021e505b47b5df332a3cdf95e
Claudio Jolowicz <cj@dealloc.org>
2015-09-02 19:56:01 +0200

    Expand backend tests.

--- wald/test/test_backend.py
+++ wald/test/test_backend.py
@@ -12,9 +12,19 @@ def session():
     return backend.create_session()
 
 
-def test_node(session):
-    '''Test wald.backend.Node.'''
+def test_add_node_to_session(session):
+    '''Test adding node to session.'''
     from wald.backend import Node
     node = Node(name='foobar', content='lorem ipsum dolor')
     session.add(node)
     session.commit()
+    assert node.node_id == 1
+
+
+def test_create_node():
+    '''Test creating node.'''
+    from wald.backend import Node
+    node = Node(name='foobar', content='lorem ipsum dolor')
+    assert node.name == 'foobar'
+    assert node.content == 'lorem ipsum dolor'
+    assert node.node_id is None
