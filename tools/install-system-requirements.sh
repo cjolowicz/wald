6a1ebc55ff7fb6ce78305545c9047f1ff3410ede
Claudio Jolowicz <cj@dealloc.org>
2015-09-05 18:23:25 +0200

    Install pyenv from Github.

--- tools/install-system-requirements.sh
+++ tools/install-system-requirements.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 case $(uname) in Darwin)
+    url=https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer
+    curl -L $url | bash
+
     url=https://raw.githubusercontent.com/Homebrew/install/master/install
     ruby -e "$(curl -fsSL $url)"
 
-    brew install pyenv pyenv-virtualenv pyenv-which-ext
     brew install pandoc
 esac
